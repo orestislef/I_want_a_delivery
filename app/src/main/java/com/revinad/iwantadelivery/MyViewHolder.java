f55b453e4fcdac1a01f2d187b94294b5fe5d0724
orestislef <orestis_94@hotmail.com>
2020-12-09 07:06:39 +0200

    all working *TODO: who sees what *Added: onMyWayUsername Database update when onMyWayCB.isChecked(UsernameV)  and completeDate when completeCB checked  *Added: TODO  *Added logic to CheckButton completeCB only clickable when coming is checked

--- app/src/main/java/com/revinad/iwantadelivery/MyViewHolder.java
+++ app/src/main/java/com/revinad/iwantadelivery/MyViewHolder.java
@@ -27,7 +27,7 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
     private final String TAG = "MyViewHolder";
 
     CircleImageView profileImage;
-    TextView username, timeAgo, postDesc;
+    TextView username, timeAgo, postDesc, onMyWayUsername, completeDate;
     CardView cardView;
     ConstraintLayout singleViewPostConstraint;
     CheckBox completeCB, onMyWayCB;
@@ -46,13 +46,13 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
         completeCB = itemView.findViewById(R.id.completedCB);
         onMyWayCB = itemView.findViewById(R.id.onMyWayCB);
         mapBtn = itemView.findViewById(R.id.mapBtn);
+        onMyWayUsername = itemView.findViewById(R.id.onMyWayUsername);
+        completeDate = itemView.findViewById(R.id.completeDate);
 
         completeCB.setClickable(false);
     }
 
     public void initCB(String postKey, DatabaseReference postRef, Context context, String mUsername, String mDate) {
-        //TODO: if onMyWayCB.isChecked(false) -> completeCB.setClickable(false) *need to +id the view
-        //      if onMyWayCB.isChecked(true) -> completeCB.setClickable(true)
 
         onMyWayCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -61,12 +61,15 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
                 hashMap.put(context.getString(R.string.ref_posts_on_my_way), isChecked);
                 hashMap.put(context.getString(R.string.ref_post_username_of_on_my_way), mUsername);
 
+                onMyWayUsername.setText(mUsername);
+
                 postRef.child(postKey).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             onMyWayCB.setClickable(!isChecked);
                             completeCB.setClickable(isChecked);
+
                             Log.d(TAG, "onComplete: onMyWayCB: " + isChecked);
                         }
                     }
@@ -82,11 +85,14 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
                 hashMap.put(context.getString(R.string.ref_posts_completed), isChecked);
                 hashMap.put(context.getString(R.string.ref_post_completed_date), mDate);
 
+                completeDate.setText(mDate);
+
                 postRef.child(postKey).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             completeCB.setClickable(!isChecked);
+
                             Log.d(TAG, "onComplete: completeCB: " + isChecked);
                         }
                     }
