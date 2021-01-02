395b302af9fd468f167cd37660d6ee3640595bd4
orest <orestis_94@hotmail.com>
2021-01-02 09:48:21 +0200

    working green red light and count online users

--- app/src/main/java/com/revinad/iwantadelivery/MainActivity.java
+++ app/src/main/java/com/revinad/iwantadelivery/MainActivity.java
@@ -22,7 +22,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
